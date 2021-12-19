758003dff3960655fe10c887c51d76531de5d606
Robin Blom <robin.blom@hotmail.se>
2021-12-19 21:44:24 +0100

    Commit two

--- src/main/java/DotComBust.java
+++ src/main/java/DotComBust.java
@@ -2,16 +2,8 @@ import java.util.ArrayList;
 
 public class DotComBust {
 
-    // The DotComBust-class has three main jobs.
-    // - Set up the game.
-    // - Play the game until the DotCom's are dead.
-    // - End the game.
-    // We could map those three jobs into three methods.
-    // But we choose to split the "Play the game"-method into two methods. (startPlaying() & checkUserGuess())
-    // That makes it easier to test the functionality.
-
-    // We declare and initialize the variables we'll need.
     private GameHelper helper = new GameHelper();
+    // This ArrayList will contain all the DotCom-objects.
     private ArrayList<DotCom> dotComList = new ArrayList<DotCom>();
     private int numOfGuesses = 0;
 
@@ -19,7 +11,6 @@ public class DotComBust {
     // Creates and initializes the DotCom-objects with names and locations. Displays brief instructions to the user.
     public void setUpGame(){
 
-        // We make three DotCom-objects, give them names, and stick them in the ArrayList.
         DotCom one = new DotCom();
         one.setName("Ebay.com");
         DotCom two = new DotCom();
@@ -27,77 +18,69 @@ public class DotComBust {
         DotCom three = new DotCom();
         three.setName("Twitter.com");
 
+        // Add the objects the ArrayList
         dotComList.add(one);
         dotComList.add(two);
         dotComList.add(three);
 
-        // We print brief instructions for the user.
+        // Print brief instructions for the user
         System.out.println("Your goal is to sink three dot coms.");
         System.out.println("Ebay.com, Facebook.com, Twitter.com");
         System.out.println("Try to sink them all in the fewest number of guesses.");
         System.out.println("A-G / 0-6");
 
-        // Repeat with each DotCom in the list.
+        // Repeat with each DotCom-object in the ArrayList.
         for(DotCom dotComToSet : dotComList){
 
-            ArrayList<String> newLocation = helper.placeDotCom(3);      // We ask the helper for a randomly selected DotCom location.
+            // Each location cell should be 3 cells large and stored in an ArrayList
+            ArrayList<String> newLocation = helper.placeDotCom(3);
 
-            // - SET the location for each DotCom based on the result of the placeDotCom() call.
-            dotComToSet.setLocationCells(newLocation);                          // We call the setter method on this DotCom to give it the location we just got from the helper.
+            // We use the randomized cells from the array above to set each object's locationCells
+            dotComToSet.setLocationCells(newLocation);
         }
-
     }
 
     // Starts the game. Repeats while there are still DotCom's in the ArrayList. Gets user input and checks that input against each DotCom's location.
     public void startPlaying(){
 
+        // Repeat as long as the DotCom list is NOT empty.
+        while(!dotComList.isEmpty()){
 
-        while(!dotComList.isEmpty()){       // Repeat as long as the DotCom list is NOT empty.
+            // Get user input
+            String userGuess = helper.getUserInput("Enter your guess: ");
 
-            // Get user input by calling the helper.getUserInput(). (Returns a String with users input).
-
-            String userGuess = helper.getUserInput("Enter your guess: ");       // Get the user input.
-
-            // Evaluate the user's input/guess by sending it to the checkUserGuess()-method.
-
-            checkUserGuess(userGuess);      // Call our own checkUserGuess()-method.
-
-
+            // Checks if the users guess is a hit or a miss.
+            checkUserGuess(userGuess);
         }
 
         // When the dotComList is empty, we jump out of the While-loop and the game is over.
-        finishGame();       // Call our own finishGame()-method.
-
+        finishGame();
     }
 
-    // Find out if there's a hit (and kill) on any DotCom.
+    // Check the users guess to find out if there's a hit, miss or kill on any DotCom
     public void checkUserGuess(String userGuess){
 
-        numOfGuesses++;             // We increment the number of guesses the user has made.
-        String result = "Miss!";    // Assume it is a "miss", unless told otherwise.
+        numOfGuesses++;
+        String result = "Miss!";
 
-        // Repeat with all DotCom's in the list.
+        // Repeat with each DotCom-object in the ArrayList.
         for(DotCom dotComToTest : dotComList){
 
-            // Ask the DotCom to check the user guess, looking for a "hit" (or kill)
-            result = dotComToTest.checkYourself(userGuess);     // We call the checkYourself()-method inside the DotCom-class on every DotCom in the arrayList.
+            // Check if the userGuess is a hit or a miss
+            result = dotComToTest.checkYourself(userGuess);
 
-            // Set the result variable to "Hit!" or "Kill!" if appropriate.
             if(result.equals("Hit!")){
-                break;      // Get out of the loop early, no point in testing the others.
+                break;      // Since it is a "Hit" we don't need to check the other objects.
             }
 
+            // If it is a "Kill" we remove the object from the ArrayList
             if(result.equals("Kill!")){
-                // Remove the DotCom from the dotComList.
-                dotComList.remove(dotComToTest);    // This guy is dead, so take him out of the DotCom's list.
-                break;                              // Then get out of the loop.
+                dotComList.remove(dotComToTest);
+                break;
             }
-
         }
-        System.out.println("It was a " + result);   // Print the result for the user.
+        System.out.println("It was a " + result);
         System.out.println("\n");
-
-
     }
 
     // Print a message telling the user how he did in the game.
@@ -111,7 +94,5 @@ public class DotComBust {
             System.out.println("Took you long enough. " + numOfGuesses + " guesses");
             System.out.println("Fish are dancing with your options.");
         }
-
     }
-
 }
